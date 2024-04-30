5fec9d1775339ce158e59b0108359a7a67c7b774
Andres <rixco1998@hotmail.com>
2024-04-30 13:37:14 -0500

    footer finished

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,54 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img alt="Vuetify Logo" class="shrink mr-2" contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" />
-
-        <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png" width="100" />
-      </div>
-      <v-spacer></v-spacer>
-      <v-btn to="/" color="white" plain rounded>
-        Home
-      </v-btn>
-      <v-btn to="/about" color="white" plain rounded>
-        About
-      </v-btn>
-      <v-btn to="/data" color="white" plain rounded>
-        Data
-      </v-btn>
-
+      <!-- App Bar content -->
     </v-app-bar>
 
     <v-main>
       <router-view />
     </v-main>
+
     <v-footer color="primary lighten-1" padless>
-      <v-card flat tile class="primary lighten-1 text-center" id="App" style="max-width: 800px;">
-        <v-card-text class="white--text">
-          <ul>
-            <!-- Render ecosystem links -->
-            <li v-for="(item, index) in ecosystem" :key="index">
-              <a :href="item.href" class="white--text">{{ item.text }}</a>
-            </li>
-          </ul>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-text class="white--text">
-          <ul>
-            <!-- Render important links -->
-            <li v-for="(item, index) in importantLinks" :key="index">
-              <a :href="item.href" class="white--text">{{ item.text }}</a>
-            </li>
-          </ul>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-text class="white--text">
-          <ul>
-            <!-- Render 'what's next' links -->
-            <li v-for="(item, index) in whatsNext" :key="index">
-              <a :href="item.href" class="white--text">{{ item.text }}</a>
-            </li>
-          </ul>
-        </v-card-text>
-      </v-card>
+      <v-row justify="center">
+        <v-col cols="12" sm="8" md="6" lg="4">
+          <v-card flat tile class="primary lighten-1 text-center" id="App" style="max-width: 100%;">
+            <v-card-text class="white--text">
+              <ul class="footer-list">
+                <!-- Render ecosystem links -->
+                <li v-for="(item, index) in ecosystem" :key="index">
+                  <a :href="item.href" class="white--text">{{ item.text }}</a>
+                </li>
+              </ul>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12" sm="8" md="6" lg="4">
+          <v-card flat tile class="primary lighten-1 text-center" id="App" style="max-width: 100%;">
+            <v-card-text class="white--text">
+              <ul class="footer-list">
+                <!-- Render important links -->
+                <li v-for="(item, index) in importantLinks" :key="index">
+                  <a :href="item.href" class="white--text">{{ item.text }}</a>
+                </li>
+              </ul>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12" sm="8" md="6" lg="4">
+          <v-card flat tile class="primary lighten-1 text-center" id="App" style="max-width: 100%;">
+            <v-card-text class="white--text">
+              <ul class="footer-list">
+                <!-- Render 'what's next' links -->
+                <li v-for="(item, index) in whatsNext" :key="index">
+                  <a :href="item.href" class="white--text">{{ item.text }}</a>
+                </li>
+              </ul>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-footer>
   </v-app>
 </template>
@@ -115,3 +111,10 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+.footer-list {
+  list-style-type: none; /* Remove bullets */
+  padding-left: 0; /* Remove left padding */
+}
+</style>
