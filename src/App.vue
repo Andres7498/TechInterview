33f3d0061e51ef05a3dd137430aa43c868faa685
Andres <rixco1998@hotmail.com>
2024-04-30 11:15:15 -0500

    Proyect seted an all the requirements done

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,45 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+        <v-img alt="Vuetify Logo" class="shrink mr-2" contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png" width="100" />
       </div>
-
       <v-spacer></v-spacer>
+      <v-btn to="/" color="white" plain rounded>
+        Home
+      </v-btn>
+      <v-btn to="/about" color="white" plain rounded>
+        About
+      </v-btn>
+      <v-btn to="/data" color="white" plain rounded>
+        Data
+      </v-btn>
+
     </v-app-bar>
 
     <v-main>
       <router-view />
     </v-main>
+    <v-footer dark padless>
+      <v-card flat tile class="indigo lighten-1 white--text text-center">
+        <v-card-text>
+        Titulo
+        </v-card-text>
+
+        <v-card-text class="white--text pt-0">
+          Contenido
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>All rigths reserved</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
@@ -35,7 +49,56 @@ export default {
   name: "App",
 
   data: () => ({
-    //
+    ecosystem: [
+      {
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader",
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify",
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify",
+      },
+    ],
+    importantLinks: [
+      {
+        text: "Documentation",
+        href: "https://vuetifyjs.com",
+      },
+      {
+        text: "Chat",
+        href: "https://community.vuetifyjs.com",
+      },
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify",
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs",
+      },
+      {
+        text: "Articles",
+        href: "https://medium.com/vuetify",
+      },
+    ],
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer",
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+      },
+    ],
   }),
 };
 </script>
--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,27 +1,63 @@
 <template>
   <v-container>
     <v-row>
-        <v-card>
-            <v-card-title>
-                Data Table
-            </v-card-title>
-            <v-card-subtitle><p>Uh oh. There is nothing there. Using the data at <a href="https://random-data-api.com/">https://random-data-api.com/</a>, populate the table
-      below with headers and make it searchable. Also, expand the table to full width of the container. Let Bob know when complete.</p></v-card-subtitle>
-    <v-data-table>
-
-    </v-data-table>
-    </v-card>
+      <v-card>
+        <v-card-title>
+          Random beers
+        </v-card-title>
+        <v-card-subtitle>
+          <p>Uh oh. There is nothing there. Using the data at <a
+              href="https://random-data-api.com/">https://random-data-api.com/</a>, populate the table
+            below with headers and make it searchable. Also, expand the table to full width of the container. Let Bob
+            know when complete.</p>
+        </v-card-subtitle>
+        <v-data-table :headers="headers" :items="showedBeers" :items-per-page="10" width="100">
+          <template #top>
+            <v-text-field v-model="searchString" label="Search" />
+          </template>
+        </v-data-table>
+      </v-card>
     </v-row>
     <v-row>
-      
+
     </v-row>
   </v-container>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-
   data: () => ({
+    headers: [
+      { text: 'Name', value: 'name' },
+      { text: 'Alcohol', value: 'alcohol' },
+      { text: 'Style', value: 'style' },
+      { text: 'Brand', value: 'brand' },
+    ],
+    beers: [],
+    searchString: '',
   }),
+  computed: {
+    showedBeers() {
+      if (this.searchString.length == 0) {
+        return this.beers;
+      } else {
+        return this.beers.filter((beer) => beer.name.includes(this.searchString) ||
+          beer.alcohol.includes(this.searchString) ||
+          beer.style.includes(this.searchString) ||
+          beer.brand.includes(this.searchString))
+      }
+    }
+  },
+  methods: {
+    getRandomData() {
+      axios.get("https://random-data-api.com/api/v2/beers?size=50").then((response) => {
+        this.beers = response.data;
+      })
+    },
+  },
+  mounted() {
+    this.getRandomData();
+  }
 };
 </script>
